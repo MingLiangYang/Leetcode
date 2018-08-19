e0631263c74902a76162001cd733f575da1bcadb
ML_Yang <453055051@qq.com>
2018-08-19 17:25:51 +0800

    two method--two pointers
    two pointers

--- delete elements.cpp
+++ delete elements.cpp
@@ -32,6 +32,21 @@ public:
 	}
 };
 
+class Solution {
+public:
+	int removeElement(vector<int>& nums, int val) {
+        int j=0;
+	    for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        return j;
+	}
+};
+
+
 
 int main(){
 	Solution s;
@@ -55,3 +70,7 @@ int main(){
 	while (1);
 	return 0;
 }
+
+
+
+
