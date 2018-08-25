fc05862134ae49531b3aa5293f8f12e58e5ac1f6
ML_Yang <453055051@qq.com>
2018-08-25 20:53:47 +0800

    Update 4Sum.cpp

--- 4Sum.cpp
+++ 4Sum.cpp
@@ -1,3 +1,11 @@
+/*
+给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，
+
+使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+
+反思与提高：
+	This problem gave me an idea of solutions to Nsum.
+*/
 #include<iostream>
 #include<stdio.h>
 #include<vector>
