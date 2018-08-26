2ce37fb89f508ab0f12838b1b7dbfffcd984adf1
ML_Yang <453055051@qq.com>
2018-08-26 21:03:11 +0800

    First commit useful but ugly
    useful but ugly! Some details need to be optimization.

---
+++ Longest Consecutive Sequence.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_map>
+//#include<hash_map>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int longestConsecutive(vector<int>& nums) {
+		map<int, int> m;
+		int res=0;
+		for (int i = 0; i < nums.size(); i++){
+			int x = nums[i];
+			if (!m.count(x)){
+				int sum=0;
+				if (m.count(x - 1)){
+					sum+=m.find(x-1)->second;
+				}
+				if (m.count(x + 1)){
+					sum += m.find(x + 1)->second;
+				}
+				sum++;
+				m.insert(pair<int, int>(x,sum));
+				if (m.find(x - 1) != m.end()){
+					m[x - m.find(x-1)->second] = sum;
+				}
+				if (m.find(x + 1) != m.end()){
+					m[x + m.find(x + 1)->second] = sum;
+				}
+				if (sum > res) res = sum;
+
+			}
+		}
+		return res;
+	}
+};
+int main(){
+	Solution s;
+	vector<int> input = { 0,3,7,2,5,8,4,6,0,1 };
+	int n=6;
+	int a = s.longestConsecutive(input);
+	
+	cout << a;
+
+	while (1);
+	return 0;
+}
