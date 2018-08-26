ac0d1b416caf794d9c6cec7457b5d09c83520588
ML_Yang <453055051@qq.com>
2018-08-26 21:21:59 +0800

    let code more beautiful but not efficient
    A method using STL map O(nlogn) time complexity not good enough! 
    Considering Using STL unordered_map---Hash_map

--- Longest Consecutive Sequence.cpp
+++ Longest Consecutive Sequence.cpp
@@ -15,6 +15,34 @@ public:
 		int res=0;
 		for (int i = 0; i < nums.size(); i++){
 			int x = nums[i];
+			int left, right;
+			if (!m.count(x)){
+				int sum=0;
+				left = m.count(x - 1) ? m.find(x - 1)->second : 0;
+				right = m.count(x + 1) ? m.find(x + 1)->second : 0;
+				sum = left + right + 1;
+
+				m.insert(pair<int, int>(x,sum));
+
+				if (left) m[x - left] = sum;
+				if(right) m[x + right] = sum;
+			
+				if (sum > res) res = sum;
+
+			}
+		}
+		return res;
+	}
+};
+
+/*
+class Solution {
+public:
+	int longestConsecutive(vector<int>& nums) {
+		map<int, int> m;
+		int res=0;
+		for (int i = 0; i < nums.size(); i++){
+			int x = nums[i];
 			if (!m.count(x)){
 				int sum=0;
 				if (m.count(x - 1)){
@@ -38,6 +66,7 @@ public:
 		return res;
 	}
 };
+*/
 int main(){
 	Solution s;
 	vector<int> input = { 0,3,7,2,5,8,4,6,0,1 };
