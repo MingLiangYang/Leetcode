a7148104d4af031c5c383e74ca2602495ee9e1d2
ML_Yang <453055051@qq.com>
2018-08-27 23:00:07 +0800

    Update duplicate element .cpp

--- duplicate element .cpp
+++ duplicate element .cpp
@@ -1,41 +1,14 @@
-#include<iostream>
-#include<stdio.h>
-#include<vector>
-#include<set>
-#include<map>
-//#include<hash_map>
-#include<algorithm>
-using namespace std;
-
+/*
+Using set<int> to judge duplicate element. 
+*/
 class Solution {
 public:
 	bool containsDuplicate(vector<int>& nums) {
-
+		set<int> duplicate;
+		for (int i = 0; i < nums.size(); i++){
+			pair<set<int>::iterator,bool> res=duplicate.insert(nums[i]);
+			if (!res.second) return true;
+		}
+		return false;
 	}
 };
-
-int main(){
-	Solution s;
-	vector<int> input = { -2, -1, 0, 0, 1, 2 };
-	int n;
-	//hash_map<int, int> hm;
-	int member;
-	int target;
-	//cin >> n;
-	//n = 6;
-	/*
-	for (int i = 0; i < n; i++){
-	cin >> member;
-	input.push_back(member);
-	}
-	*/
-	//cin >> target;
-	//int k;
-	//cin >> k;
-	//int index=0;
-	vector<vector<int>> res;
-	int c = s.containsDuplicate(input);
-	cout << c << endl;
-	while (1);
-	return 0;
-}
