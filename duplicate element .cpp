3902cf1f6b1e78ee28bf1c6d57b6ad96cdbbd350
ML_Yang <453055051@qq.com>
2018-08-27 22:57:31 +0800

    Add files via upload
    A pretty simple question duplicate element using STL set is completed because of short of time today.

---
+++ duplicate element .cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<set>
+#include<map>
+//#include<hash_map>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+	bool containsDuplicate(vector<int>& nums) {
+
+	}
+};
+
+int main(){
+	Solution s;
+	vector<int> input = { -2, -1, 0, 0, 1, 2 };
+	int n;
+	//hash_map<int, int> hm;
+	int member;
+	int target;
+	//cin >> n;
+	//n = 6;
+	/*
+	for (int i = 0; i < n; i++){
+	cin >> member;
+	input.push_back(member);
+	}
+	*/
+	//cin >> target;
+	//int k;
+	//cin >> k;
+	//int index=0;
+	vector<vector<int>> res;
+	int c = s.containsDuplicate(input);
+	cout << c << endl;
+	while (1);
+	return 0;
+}
