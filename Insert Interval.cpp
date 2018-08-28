c973649aabd7511fe3f1b7936df264f74b08f6f3
ML_Yang <453055051@qq.com>
2018-08-28 17:00:00 +0800

    Update Insert Interval.cpp
    add some comments

--- Insert Interval.cpp
+++ Insert Interval.cpp
@@ -1,3 +1,14 @@
+/*
+给出一个无重叠的 ，按照区间起始端点排序的区间列表。
+
+在列表中插入一个新的区间，你需要确保列表中的区间仍然有序且不重叠（如果有必要的话，可以合并区间）。
+
+输入: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+
+输出: [[1,2],[3,10],[12,16]]
+
+思路：顺序查找，考虑新区间的前端和旧区间的末端，新区间的末端和旧区间的前端的关系。注意边界条件和区间个数。
+*/
 #include<iostream>
 #include<stdio.h>
 #include<vector>
@@ -23,27 +34,27 @@ public:
 		Interval nowinterval(newInterval);
 		vector<Interval> res;
 		
-		if (intervals.empty()){
+		if (intervals.empty()){ //special case
 			res.push_back(newInterval);
 			return res;
 		}
 		
-		while (intervals[i].end < newInterval.start && i < intervals.size()){
-			res.push_back(intervals[i]);
+		while (intervals[i].end < newInterval.start && i < intervals.size()){ //i<size 
+			res.push_back(intervals[i]);                                  //end < start_new: push_back 
 			//if (i + 1 == intervals.size())break;
 			i++; 
 		}
-		while (intervals[i].start <= newInterval.end && i < intervals.size()){
+		while (intervals[i].start <= newInterval.end && i < intervals.size()){ //update the newinterval's start and end 
 			int a,b;
-			a = intervals[i].start < nowinterval.start ? intervals[i].start : nowinterval.start;
-			b = intervals[i].end > nowinterval.end ? intervals[i].end : nowinterval.end;
-			if (a < nowinterval.start) nowinterval.start = a;
+			a = intervals[i].start < nowinterval.start ? intervals[i].start : nowinterval.start; // get the min start value
+			b = intervals[i].end > nowinterval.end ? intervals[i].end : nowinterval.end;   //get the max end value
+			if (a < nowinterval.start) nowinterval.start = a;    //The update the min and max value and update newInterval
 			if (b > nowinterval.end) nowinterval.end = b;
 			//if (i + 1 == intervals.size())break;
 			i++;
 		}
-		res.push_back(nowinterval);
-		for (int j = i; j < intervals.size(); j++){
+		res.push_back(nowinterval);   // put the newInterval 
+		for (int j = i; j < intervals.size(); j++){  // left elements 
 			res.push_back(intervals[j]);
 		}
 		return res;
