8ff70571e1e2573c3130ab398e49dde72ceba83e
ML_Yang <453055051@qq.com>
2018-09-10 17:31:10 +0800

    Update Rotate List.cpp
    add comments

--- linked list/Rotate List.cpp
+++ linked list/Rotate List.cpp
@@ -1,28 +1,16 @@
-#include<iostream>
-#include<stdio.h>
-#include<vector>
-#include<set>
-#include<map>
-#include<string>
-//#include<hash_map>
-#include<algorithm>
-//#include<iostream>
-using namespace std;
+/*
+给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
 
-
-/**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode(int x) : val(x), next(NULL) {}
-* };
+说明：不允许修改给定的链表。
+思路：
+1、判断有无环：快慢指针，追及问题。快指针是慢指针速度二倍，相遇时复杂度O(N)，如果fast==NULL则无环。
+2、找环入口：快指针，慢指针分别赋值为head和相遇点指针，速度均为1。再次相遇时为环入口。
+证明：
+1、追及问题，慢指针顶多走完一遍，故O(N)复杂度。
+2、2S=S+nr(n==1)且s=x+y 得出x=nr-y 故重新归位后，慢指针（此时速度相同）到达x远后，快指针走了nr-y，即反向走了y，正是入口点。
+其中：s为慢指针路程，n为常数这里固定为1,r为环的长度,x为head到环入口的长度,y为慢指针第一次进入环时距离快指针的距离。
 */
-struct ListNode{
-	int val;
-	ListNode * next;
-	ListNode(int x) :val(x), next(NULL){}
-};
+
 class Solution {
 public:
 	ListNode *detectCycle(ListNode *head) {
@@ -34,7 +22,7 @@ public:
 		else{
 			return findGate(head, en);
 		}
-
+		
 	}
 private:
 	ListNode * detectIsCycle(ListNode * head){
@@ -45,7 +33,7 @@ private:
 		}
 		else return NULL;
 
-		while (fast != NULL && fast->next != NULL && slow != fast){
+		while (fast != NULL && fast->next!=NULL && slow != fast){
 			slow = slow->next;
 			fast = fast->next->next;
 		}
@@ -56,18 +44,13 @@ private:
 			return fast;
 		}
 	}
-	ListNode * findGate(ListNode * head, ListNode * en){
+	ListNode * findGate(ListNode * head , ListNode * en){
 		ListNode * slow = head;
 		ListNode * fast = en;
-		while (fast != NULL && slow != fast){
+		while (fast!=NULL && slow != fast){
 			slow = slow->next;
 			fast = fast->next;
 		}
 		return fast;
 	}
-}; int main(){
-	Solution s;
-	s.detectCycle(NULL);
-	while (1);
-	return 0;
-}
+};
